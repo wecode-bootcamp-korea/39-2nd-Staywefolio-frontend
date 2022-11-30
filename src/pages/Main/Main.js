a08640ecd49092a504c8920cb9838f63bb8beb37
김동기 <voicek@gimdong-giui-MacBookAir.local>
2022-11-30 20:17:38 +0900

    Add:main page 완료
    Add:main page 완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,67 @@
 import React from 'react';
+import Slider from 'react-slick';
+import Vibes from './Vibes';
+import styled from 'styled-components';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function Main() {
-  return <div>Main</div>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <>
+      <Title>
+        TIME TO STAY
+        <br />
+        감성에 머무를 시간
+      </Title>
+      <Top {...settings}>
+        <RoomImg src="/images/kimdongki/one-room.jpg" />
+        <RoomImg src="/images/kimdongki/kitchen.jpg" />
+        <RoomImg src="/images/kimdongki/camping.jpg" />
+        <RoomImg src="/images/kimdongki/jeju.jpg" />
+        <RoomImg src="/images/kimdongki/swimming.jpg" />
+        <RoomImg src="/images/kimdongki/swim.jpg" />
+      </Top>
+      <Vibes />
+    </>
+  );
 }
+
+const Top = styled(Slider)`
+  .slick-prev {
+    left: 10px;
+    z-index: 1;
+    &::before {
+      font-size: 35px;
+    }
+  }
+  .slick-next {
+    right: 20px;
+    opacity: 1;
+    &::before {
+      font-size: 35px;
+    }
+  }
+  .slick-dots {
+    bottom: 10px;
+  }
+`;
+
+const Title = styled.div`
+  text-align: center;
+  margin-bottom: 50px;
+  font-size: 12px;
+  letter-spacing: 11px;
+  font-weight: 700;
+  line-height: 30px;
+`;
+const RoomImg = styled.img`
+  max-width: 100%;
+  max-height: 500px;
+`;
